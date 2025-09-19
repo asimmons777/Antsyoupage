6f3164feeed6ad7cc6b6e1376e2488a69d00d17a
asimmons777 <asimmons@icstars.org>
2025-09-19 00:12:57 -0500

    Putting content on resume page

--- youpage-app/src/pages/Experience.tsx
+++ youpage-app/src/pages/Experience.tsx
@@ -13,11 +13,25 @@ const Experience: React.FC = () => {
                 related to my career and personal life that I think are worth sharing. This page will include experiences with me
                 building businesses, working with others, personal growth,
                 writing code, and more.</p>
-                <h2>IC.stars</h2>
+                <h2>Communities</h2>
+                <h3>IC.stars</h3>
                 <p>IC.stars is a non-profit organization that I took 
                 part in from May 2025 to August 2025. Their main goal is to empower underprivileged people in the tech industry by teaching them Tech skills, providing resources, mentorship, and networking opportunities.IC.stars helped me grow my skills in tech, business, and leadership, which I believe will be invaluable in my future endeavors. while there I worked on tech projects such as full stack applications with a team of interns like myself and also worked on solo applications.These experiences taught me things like collaboration, problem-solving, and adaptability. My time at IC.stars was incredibly rewarding and I am grateful for the opportunity to contribute to such a meaningful cause.</p>
-                <h2>amway|status:notstarted</h2>
-
+                <h2> Business Ventures </h2>
+                <p>Throughout my life ive always had a heart and mindset of an entreprenuer and have always been interested in starting businesses. 
+                To this day I still have that mindset and am always looking for new 
+                oppurtunities to add new pipelies of income to my portfolio of businesses and investments.</p>
+                <h3>Codehaven</h3>
+                <p>CodeHaven is a software development company I plan on starting 
+                    in the near future. The purpose of me making this company was
+                    mainly to represent my love for tech and programing and to also
+                    adapt to the future. This company will mainly focus on AI automation and building applications for businesses.
+                </p>
+                <h3>Amway| status: Active</h3>
+                <p>
+                    Amway is a network marketing company that im currently dabbling in so that i can learn more about sales and marketing. This experience so far has given me lots of insight and knowledge on how to operate my own businesses.
+                </p>
+                <h3></h3>
                 </main>
             <Footer/>
             </>
--- youpage-app/src/pages/Resume.tsx
+++ youpage-app/src/pages/Resume.tsx
@@ -1,6 +1,6 @@
 import React from "react";
 import NavBar from "../components/NavBar";
-import footer from "../components/Footer";
+import Footer from "../components/Footer";
 import "./landingpage.css";
 
 const Resume: React.FC = () => {
@@ -8,8 +8,56 @@ const Resume: React.FC = () => {
     <div>
       <NavBar />
       <h1>Resume</h1>
-      <p>Content of the Resume page goes here.</p>
-      <footer />
+      <p>beneath this text you can download a my base resume.</p>
+      <button>Download Resume</button>
+      <h2>Technical skills and practices</h2>
+      <ul>
+        <li>JavaScript</li>
+        <li>TypeScript</li>
+        <li>React</li>
+        <li>Node.js</li>
+        <li>Express.js</li>
+        <li>HTML</li>
+        <li>CSS</li>
+        <li>Git</li>
+        <li>SQL</li>
+        <li>Restful APIs</li>
+        <li>Excel</li>
+        <li>CI/CD</li>
+        <li>SDLC</li>
+        <li>Agile methodology</li>
+        <li>Version control</li>
+      </ul>
+      <h2>Soft skills</h2>
+      <ul>
+        <li>Communication</li>
+        <li>Problem-solving</li>
+        <li>Adaptability</li>
+        <li>Collaboration</li>
+        <li>Strategic thinking</li>
+        <li>Curiosity</li>
+      </ul>
+      <h2>Skills Working on</h2>
+      <ul>
+        <li>Leadership</li>
+        <li>Sales</li>
+        <li>Marketing</li>
+        <li>AI automation</li>
+        <li>Time management</li>
+      </ul>
+      <h2>occupations Big and small</h2>
+      <p>I've worked jobs since I was a teenager, they werent big jobs but I think that 
+        they helped me understand money at a young age and the value of putting your time 
+        into something to recieve money in return.
+      </p>
+      <ul>
+        <li>Chef</li>
+        <li>Gardener</li>
+        <li>Canvasser</li>
+        <li>Retail Associate</li>
+        <li>Tech intern</li>
+      </ul>
+      <Footer />
     </div>
   );
 };
